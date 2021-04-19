78dbf41afe86a8fbaf4a31f3351b0055893c8caf
Sebastian Mederer <sebastian.mederer03@htl-salzburg.ac.at>
2021-04-19 14:15:49 +0200

    spaceCounter

---
+++ Assets/MyGame/scripts/Space.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space : MonoBehaviour
+{
+    private int counter = 0;
+    public KeyCode space;
+
+
+    void Start()
+    {
+        counter = 0; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("spacepressed");
+            counter++;
+        }
+        if (counter > 10)
+        {
+            Debug.Log("wixxa");
+        }
+    }
+    void 
+    
+}
